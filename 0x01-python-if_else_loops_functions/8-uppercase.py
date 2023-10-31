b88acd91e48e3f251c0e27f3240186f48cd83b51
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-11-01 01:48:18 +0200

    upping

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(0, len(str)):
+        if i == len(str):
+            print(str[i])
+        else:
+         print("{}".format(str[i] - 32), end="")
