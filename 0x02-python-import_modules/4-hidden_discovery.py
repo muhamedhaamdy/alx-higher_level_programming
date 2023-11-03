06b72432610e2b1bd11e87f705e181572ed23b14
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-11-03 04:28:44 +0200

    upping

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    print("my_secret_santa")
-    print("print_hidden")
-    print("print_school")
+    import hidden_4
+    for i in dir(hidden_4):
+        if i[0] != '_':
+            print(i)
+
