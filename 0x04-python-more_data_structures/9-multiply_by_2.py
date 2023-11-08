d2686d1248a521cf9ffbb0326597273e4c7ed2b9
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-11-08 16:08:14 +0200

    upping

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     new_dic = a_dictionary.copy()
-    for i in new_dic.values():
-        i *= 2
+    for i in new_dic:
+        new_dic[i] *= 2
+    return new_dic
