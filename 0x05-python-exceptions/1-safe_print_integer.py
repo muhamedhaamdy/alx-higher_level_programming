932612867e4e89a48342ce416c021c4882090497
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-11-20 21:14:18 +0200

    "hotfix"

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,10 +3,7 @@ def safe_print_integer(value):
     b = True
     try:
         num = int(value)
-        if float(value).is_integer():
-            print("{:d}".format(value))
-            return True
-        else:
-            return False
-    except ValueError:
+        print("{:d}".format(value))
+        return True
+    except (TypeError , ValueError):
         return False
