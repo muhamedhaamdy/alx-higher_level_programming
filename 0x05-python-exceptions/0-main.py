de05c1028e61cbbba5a19638ccaa9606dcac9e1a
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-11-20 21:10:57 +0200

    "hotfix"

--- 0x05-python-exceptions/0-main.py
+++ 0x05-python-exceptions/0-main.py
@@ -6,12 +6,12 @@ has_been_print = safe_print_integer(value)
 if not has_been_print:
     print("{} is not an integer".format(value))
 
-value = -89
+value = 999.92
 has_been_print = safe_print_integer(value)
 if not has_been_print:
     print("{} is not an integer".format(value))
 
-value = "School"
+value= "School"
 has_been_print = safe_print_integer(value)
 if not has_been_print:
     print("{} is not an integer".format(value))
--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,8 +3,11 @@ def safe_print_integer(value):
     b = True
     try:
         num = int(value)
-        print("{:d}".format(num))
+        if float(value).is_integer():
+            print("{:d}".format(value))
+            return True
+        else:
+            return False
     except ValueError:
-        b = False
-    finally:
-        return b
+        return False
+ 
