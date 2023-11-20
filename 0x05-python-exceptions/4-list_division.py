00ccc9ca703ec9ea36ec5901a5a2e4f54d6f8482
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-11-20 23:06:40 +0200

    "hotfix"

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -6,7 +6,7 @@ def list_division(my_list_1, my_list_2, list_length):
             div = my_list_1[i] / my_list_2[i]
         except TypeError:
             div = 0
-            print("Worng type")
+            print("worng type")
             continue
         except ZeroDivisionError:
             div = 0
