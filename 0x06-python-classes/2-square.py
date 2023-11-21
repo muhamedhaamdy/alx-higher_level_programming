5d79e4d4df842566f1bfacfa2ddcf093c3644447
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-11-21 22:28:11 +0200

    "hotfix"

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,11 +4,11 @@
 
 class Square:
     """ square class """
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         """ initial function """
-        if type(size) == int and size >= 0:
+        if type(size) is int and size >= 0:
             self.__size = size
-        elif type(size) == int and size < 0:
+        elif type(size) is int and size < 0:
             raise ValueError("size must be >= 0")
         else:
             raise TypeError("size must be an integer")
