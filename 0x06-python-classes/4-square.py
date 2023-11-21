84a1a4cea7ec01d008d9a55ae5c57fdfbb1d39f8
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-11-21 23:13:02 +0200

    "hotfix"

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Square """
+
+
+class Square:
+    """ square class """
+    def __init__(self, size=0):
+        """ initial function """
+        self.__size = size
+
+    @property
+    def size(self):
+        """ Getter """
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """ Setter """
+        if type(size) is int and size >= 0:
+            self.__size = size
+        elif type(size) is int and size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """ calulate the area """
+        return self.__size * self.__size
