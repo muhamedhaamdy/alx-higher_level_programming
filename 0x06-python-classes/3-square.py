f2e4c33349e48820bf20ea9bd3e7618113da7011
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-11-21 22:36:32 +0200

    "hotfix"

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Square """
+
+
+class Square:
+    """ square class """
+    def __init__(self, size=0):
+        """ initial function """
+        if type(size) is int and size >= 0:
+            self.__size = size
+        elif type(size) is int and size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """ calulate the area """
+        return self.__size * self.__size
