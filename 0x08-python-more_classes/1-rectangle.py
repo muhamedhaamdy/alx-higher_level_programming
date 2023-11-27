9cf4a2bf5ff04ae06ed42df942ba5d09dda68358
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-11-27 21:17:47 +0200

    "hotfix"

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""Hello with the start of the file"""
+
+
+class Rectangle:
+    """Rectangle class"""
+    def __init__(self, width=0, height=0):
+        """inittial function"""
+        self.__width = width
+        self.__height = height
+        
+    @property
+    def width(self):
+        """The width property."""
+        return self.__width
+    @width.setter
+    def width(self, value):
+        """width setter"""
+        if isinstance(value, int):
+            if value >= 0:
+                self.__width = value
+            else:
+                raise ValueError("width must be >= 0")
+        else:
+            raise TypeError("width must be an integer")
+    
+    @property
+    def height(self):
+        """The height property."""
+        return self.__height
+    @height.setter
+    def height(self, value):
+        """height setter"""
+        if isinstance(value, int):
+            if value >= 0:
+                self.__height = value
+            else:
+                raise ValueError("height must be >= 0")
+        else:
+            raise TypeError("height must be an integer")
+            
+
+
+
