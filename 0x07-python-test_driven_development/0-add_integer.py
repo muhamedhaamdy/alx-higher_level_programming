acbca815cf5362421d2fc5edbd586e4f5d71efe0
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-11-29 18:32:26 +0200

    "hotfix"

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -2,7 +2,7 @@
 def add_integer(a, b=98):
     """ adds two integers """
     if not isinstance(a, (int, float)):
-        raise TypeError ("a must be an integer")
+        raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
-        raise TypeError ("b must be an integer")
+        raise TypeError("b must be an integer")
     return int(a) + int(b)
