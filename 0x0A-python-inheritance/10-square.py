ed52bea628ff8dc9497e7927477c46ceac1e55c5
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-12-04 22:45:01 +0200

    "hotfix"

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""the begining of the file"""
+Rectangle = __import__('9-rectangle').BaseGeometry
+
+
+class Square(Rectangle):
+    """square class"""
+    def __init__(self, size):
+        super().integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def __str__(self):
+        return ("[Rectangle] 13/13")
