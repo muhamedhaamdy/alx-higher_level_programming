9613cd08f49206593de26541b724f2235863e5b7
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-12-09 23:13:52 +0200

    "hotfix"

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -39,3 +39,61 @@ class Rectangle(Base):
             raise TyepError("y must be an integer")
 
 
+    def area(self):
+        """return the arrea of the rectangle"""
+        return self.__widht * self.__height
+
+    def display(self):
+        """display the rectangel with the character #"""
+        for i in range(0, self.__height):
+            print('#' * self.__widht)
+
+    @property
+    def width(self):
+        """The width property."""
+        return self.__width
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+             raise ValueError("width  must be > 0")
+        self.__width = value
+    
+    @property
+    def height(self):
+        """The height property."""
+        return self.__height
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height  must be > 0")
+        self.__height = value
+    
+    @property
+    def x(self):
+        """The x property."""
+        return self.__x
+    @x.setter
+    def x(self, value):
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x  must be > 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """The y property."""
+        return self.__y
+    @y.setter
+    def y(self, value):
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y  must be > 0")
+        self.__y = value
+
+
