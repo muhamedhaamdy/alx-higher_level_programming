c196ebcb2c3496cd37e75827cb979e2b162487c0
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-12-09 15:50:55 +0200

    "hotfix"

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
+"""The base file"""
+
 
 class Base:
+    """The base class"""
     __nb_objects = 0
+
     def __init__(self, id=None):
+        """init func"""
         if id:
             self.id = id
         else:
