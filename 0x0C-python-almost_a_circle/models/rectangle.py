8a5a567b8c38e89ddd444b362fad6ef692f45ef7
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-12-09 22:47:55 +0200

    "hotfix"

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,8 +6,36 @@ from models.base import Base
 class Rectangle(Base):
     """rectangle class"""
     def __init__(self, width, height, x=0, y=0, id=None):
-        self.__widht = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
-        self.__id = super().__init__(id)
+        if isinstance(width, int):
+            if width > 0:
+                self.__widht = width
+            else:
+                raise ValueError("width must be > 0")
+        else:
+            raise TypeError("width must be an integer")
+
+        if isinstance(height, int):
+            if height > 0:
+                self.__height = height
+            else:
+                raise ValueError("height must be > 0")
+        else:
+            raise TypeError("height must be an integer")
+        
+        if isinstance(x, int):
+            if x >= 0:
+                self.__x = x
+            else:
+                raise ValueError("x  must be >= 0")
+        else:
+            raise TyepError("x must be an integer")
+        
+        if isinstance(y, int):
+            if y >= 0:
+                self.__x = y
+            else:
+                raise ValueError("y must be >= 0")
+        else:
+            raise TyepError("y must be an integer")
+
+
