c3fd60ea7375d4c9aef229e7961370676d56fb53
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-12-10 20:16:01 +0200

    "hotfix"

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,42 +6,11 @@ from models.base import Base
 class Rectangle(Base):
     """rectangle class"""
     def __init__(self, width, height, x=0, y=0, id=None):
-        if isinstance(width, int):
-            if width > 0:
-                self.__width = width
-            else:
-                raise ValueError("width must be > 0")
-        else:
-            raise TypeError("width must be an integer")
-
-        if isinstance(height, int):
-            if height > 0:
-                self.__height = height
-            else:
-                raise ValueError("height must be > 0")
-        else:
-            raise TypeError("height must be an integer")
-        
-        if isinstance(x, int):
-            if x >= 0:
-                self.__x = x
-            else:
-                raise ValueError("x  must be >= 0")
-        else:
-            raise TyepError("x must be an integer")
-        
-        if isinstance(y, int):
-            if y >= 0:
-                self.__y = y
-            else:
-                raise ValueError("y must be >= 0")
-        else:
-            raise TyepError("y must be an integer")
-
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
-
-
-
 
     def area(self):
         """return the arrea of the rectangle"""
@@ -55,8 +24,21 @@ class Rectangle(Base):
             print(" " * self.__x + '#' * self.__width)
 
     def __str__(self):
+        """returns the string representation of an object."""
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height)
-
+    
+    def update(self, *args):
+        """update the rectangle"""
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) >= 5:
+            self.y = args[4]
     @property
     def width(self):
         """The width property."""
@@ -104,5 +86,3 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y  must be > 0")
         self.__y = value
-
-
