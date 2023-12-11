782d06c348308049ce61c1b87cb7a63733f6ac36
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-12-11 19:12:49 +0200

    "hotfix"

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,6 +17,6 @@ class Base:
 
     def to_json_string(list_dictionaries):
         """ that returns the JSON string representation of list_dictionaries"""
-        if not list_dictionaries or  not len(list_dictionaries):
+        if not list_dictionaries or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
