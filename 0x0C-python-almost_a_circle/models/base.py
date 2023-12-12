88c2776eb87a9b48fe2d117d7773b34f21161a28
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-12-12 02:43:14 +0200

    "hotfix"

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -51,6 +51,7 @@ class Base:
             dummy = cls(1)
             dummy.update(**dictionary)
         return dummy
+
     @classmethod
     def load_from_file(cls):
         """returns a list of instances"""
