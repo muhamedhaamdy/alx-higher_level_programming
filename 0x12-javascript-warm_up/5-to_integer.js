bdd9fde98f13aee0d80a26e0cfdc7aa98c691e16
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-02-13 03:25:44 +0200

    "hotfix"

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -5,5 +5,5 @@ const argv = process.argv;
 if (isNaN(parseInt(argv[2]))) {
   console.log('Not a number');
 } else {
-  console.log("My number: " + parseInt(argv[2]));
+  console.log('My number: ' + parseInt(argv[2]));
 }
