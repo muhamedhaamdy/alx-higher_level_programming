34b1fc1f24a03de10baf73dcdae3b286f12f198c
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-02-13 02:29:34 +0200

    "hotfix"

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-const { argv } = require('node:process');
+const argv = process.argv;
 
 if (argv.length === 2) {
   console.log('No argument');
 } else if (argv.length === 3) {
   console.log('Argument found');
 } else {
-	console.log('Arguments found');
+  console.log('Arguments found');
 }
