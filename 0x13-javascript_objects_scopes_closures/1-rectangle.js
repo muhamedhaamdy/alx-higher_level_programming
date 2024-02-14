68a9e0742f9afb2c819f89d7c1afd5f6abc0eceb
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-02-14 23:15:18 +0200

    "hotfix"

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 
 const Rectangle = class {
-	width;
-	height;
-	constructor(w, h)
-	{
-		this.width = w;
-		this.height = h;
-	}
+  width;
+  height;
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
 };
 module.exports = Rectangle;
