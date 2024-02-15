d6bbb15a6384db67ce079bccd0cc1c68c7c8a73b
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-02-15 04:08:35 +0200

    "hotfix"

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 
-function nbOccurences (list, searchElement)
-{
+function nbOccurences (list, searchElement) {
   let c = 0;
   for (let i = 0; i < list.length; i++) {
     if (list[i] === searchElement) c++;
