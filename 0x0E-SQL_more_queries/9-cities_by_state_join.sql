2a1d3e6807fe029442466ade5c5ae1eaddd0b7d1
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-02-16 23:15:21 +0200

    "hotfix"

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- lists all cities contained in the database
 SELECT cities.id, cities.name, states.name
-FROM cities, states
+FROM cities 
 INNER JOIN states ON cities.state_id = state.id
 ORDER BY cities.id ASC;
