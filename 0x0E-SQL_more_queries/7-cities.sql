faab9b39a4aa6e25377477bc03a063d05cf67bfa
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-02-16 19:14:25 +0200

    "hotfix"

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,5 +5,5 @@ CREATE TABLE IF NOT EXISTS cities(
 	id INT AUTO_INCREMENT PRIMARY KEY, 
 	state_id INT NOT NULL,
 	name  VARCHAR(256) NOT NULL,
-	FOREIGN KEY (state_id) REFERENCES state(id)
+	FOREIGN KEY (state_id) REFERENCES hbtn_0d_us.states(id)
 );
