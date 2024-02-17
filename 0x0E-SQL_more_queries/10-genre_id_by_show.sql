8f043fd9d27cc40ce4113009e1bce1c0da1947c8
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-02-17 04:46:17 +0200

    "hotfix"

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,6 @@
 -- lists all shows contained
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM hbtn_0d_tvshows
+FROM  tv_shows
+FULL OUTER JOIN tv_shows_genres
+ON tv_shows.id = tv_shows_genres.tv_shows
 ORDER BY tv_shows.title AND tv_show_genres.genre_id;
