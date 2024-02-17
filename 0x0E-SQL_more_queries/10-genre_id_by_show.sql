b0264734202e93aaff29cea3374874f8156ae646
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-02-17 04:47:56 +0200

    "hotfix"

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,5 +2,5 @@
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM  tv_shows
 FULL OUTER JOIN tv_shows_genres
-ON tv_shows.id = tv_shows_genres.tv_shows
+ON tv_shows.id = tv_shows_genres.genre_id
 ORDER BY tv_shows.title AND tv_show_genres.genre_id;
