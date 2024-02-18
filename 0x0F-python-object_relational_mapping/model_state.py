2bee68127a25f39e296891a720dd3b124d3d5dd0
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-02-18 23:26:06 +0200

    "hotfix"

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -9,4 +9,4 @@ class State(Base):
     """state model"""
     __tablename__ = 'state'
     id = Column(Integer, Sequence('state_id_seq'), primary_key=True)
-    name = Column(String(128), nullable=False)
+    name = Column('name',String(128), nullable=False)
