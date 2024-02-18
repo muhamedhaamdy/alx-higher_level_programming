0d677650e4091fe41e8ad8687b066e37f7de69a8
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-02-19 01:40:32 +0200

    "hotfix"

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''script that lists all State object'''
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from model_state import Base, State
+import sys
+
+engine_path = 'mysql://{}:{}@localhost:3306/{}'\
+                .format(sys.argv[1], sys.argv[2], sys.argv[3])
+engine = create_engine(engine_path)
+Session = sessionmaker(bind=engine)
+session = Session()
+states = session.query(State).all()
+states
