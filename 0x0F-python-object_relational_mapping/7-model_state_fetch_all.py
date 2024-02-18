419e6dd18e5037dafb09ef779d73905f4c3708d1
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-02-19 01:42:33 +0200

    "hotfix"

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -5,11 +5,11 @@ from sqlalchemy import create_engine
 from model_state import Base, State
 import sys
 
-
-engine_path = 'mysql://{}:{}@localhost:3306/{}'\
+if __name__ == '__main__':
+    engine_path = 'mysql://{}:{}@localhost:3306/{}'\
                 .format(sys.argv[1], sys.argv[2], sys.argv[3])
-engine = create_engine(engine_path)
-Session = sessionmaker(bind=engine)
-session = Session()
-states = session.query(State).all()
-states
+    engine = create_engine(engine_path)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).all()
+    states
