6b5fdc9b0028f47c3eb7cf2ee8a4ece191b23a6f
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-02-19 01:41:21 +0200

    "hotfix"

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -5,6 +5,7 @@ from sqlalchemy import create_engine
 from model_state import Base, State
 import sys
 
+
 engine_path = 'mysql://{}:{}@localhost:3306/{}'\
                 .format(sys.argv[1], sys.argv[2], sys.argv[3])
 engine = create_engine(engine_path)
