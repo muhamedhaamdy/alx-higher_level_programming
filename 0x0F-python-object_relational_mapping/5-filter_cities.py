9ebee2fa44f7c72546b2c2df4652225aae99dfbf
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-02-18 21:57:54 +0200

    "hotfix"

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     cur.execute("SELECT cities.name\
                  FROM cities, states\
                  WHERE states.name = %s\
-                 ORDER BY cities.id;",(sys.argv[4],))
+                 ORDER BY cities.id;", (sys.argv[4],))
     rows = cur.fetchall()
     for row in rows:
         print(row)
