ff8dd6b5541d2d38669f9d55e50e52f0d1e7b1a9
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-02-18 20:31:09 +0200

    "hotfix"

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,7 +7,8 @@ if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1], password=sys.argv[2],
                          database=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name REGEXP %s ORDER BY id", (sys.argv[4],))
+    query = "SELECT * FROM states WHERE name REGEXP {} ORDER BY id".format(sys.argv[4])
+    cur.execute(query)
     rows = cur.fetchall()
     for row in rows:
         print(row)
