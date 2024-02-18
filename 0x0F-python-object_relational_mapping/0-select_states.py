81770dd58eedd6f3f7f153a29adfcabe3272854e
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-02-18 19:04:22 +0200

    "hotfix"

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,5 +3,5 @@ import MySQLdb
 import sys
 db = MySQLdb.connect(user=sys.argv[1], password=sys.argv[2], database=sys.argv[3])
 cur = db.cursor()
-cur.execute("SELECT * FORM states ORDER BY states.id")
+cur.execute("SELECT * FROM states ORDER BY id")
 rows = cur.fetchone()
