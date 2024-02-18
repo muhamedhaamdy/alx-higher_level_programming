b17da2484e593536cc5178de50ad5a5d90eca757
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-02-18 19:30:02 +0200

    "hotfix"

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""lists all states from the database with IDs in ascending order"""
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], password=sys.argv[2],
+                         database=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name REGEXP '^N';")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
