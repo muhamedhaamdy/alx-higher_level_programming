0a7c4c949f6dbff2a2dd7c8c5133a891058a59bb
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-02-18 19:19:01 +0200

    "hotfix"

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """lists all states from the database with IDs in ascending order"""
-
-
 import MySQLdb
 import sys
+
+
 db = MySQLdb.connect(user=sys.argv[1], password=sys.argv[2],
-                     database=sys.argv[3], port=3306, host = 'localhost')
+                     database=sys.argv[3], port=3306, host='localhost')
 cur = db.cursor()
 cur.execute("SELECT * FROM states ORDER BY state.id")
 rows = cur.fetchall()
