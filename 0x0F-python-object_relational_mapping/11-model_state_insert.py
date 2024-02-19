28a9d8f60cfd1aac75a378a387bbe3ca2f6ed870
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-02-19 02:29:28 +0200

    "hotfix"

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     engine = create_engine(engine_path)
     Session = sessionmaker(bind=engine)
     session = Session()
-    new_state = State(name = 'Louisiana')
+    new_state = State(name='Louisiana')
     session.add(new_state)
     session.commit()
     state = session.query(State).filter(State.name.like('Louisiana'))\
