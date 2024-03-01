bb78188854a4b46d90a26d3a918242fd46aade2e
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-03-01 03:49:32 +0200

    "hotfix"

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Technical interview preparation"""
+
+def find_peak(list_of_integers):
+    '''function that finds a peak in a list of unsorted integers'''
+
+    if not len(list_of_integers):
+        return None
+    for i in range(0, len(list_of_integers) - 1):
+        if not i and list_of_integers[i] > list_of_integers[i+1]:
+            return list_of_integers[i]
+        elif i and list_of_integers[i] > list_of_integers[i-1] and list_of_integers[i] > list_of_integers[i+1]:
+            return list_of_integers[i]
+    return list_of_integers[len(list_of_integers) - 1]
