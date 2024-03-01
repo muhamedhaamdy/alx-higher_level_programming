4b530ab0b0b6002466015380f10002f653b980f0
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-03-01 02:25:10 +0200

    "hotfix"

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #displays all HTTP methods the server will accept
-curl -s -i -X OPTIONS 0.0.0.0:5000/route_4 | grep -i 'Allow' | sed 's/Allow: //i'
+curl -s -i -X OPTIONS $1 | grep -i 'Allow' | sed 's/Allow: //i'
