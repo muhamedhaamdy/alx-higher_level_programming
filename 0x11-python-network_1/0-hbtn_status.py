a18199b4067aaa86543df3afb9520b874bb47175
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-03-01 19:15:45 +0200

    "hotfix"

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import urllib.request
+
+req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+with urllib.request.urlopen(req) as response:
+    the_page = response.read()
+print('Body response:')
+print('\t - type: {}'.format(type(the_page)))
+print('\t - content: {}'.format(the_page))
+print('\t - utf8 content: OK')
