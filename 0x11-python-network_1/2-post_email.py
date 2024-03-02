c9e0804a0dd7b9a4ef965fcbbcc0a984a8370491
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-03-02 17:22:35 +0200

    "hotfix"

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''sends a POST request to the passed URL'''
+import urllib.parse
+import urllib.request
+import sys
+
+email = {}
+url = sys.argv[1]
+email['email'] = sys.argv[2]
+
+data = urllib.parse.urlencode(email)
+data = data.encode('utf-8')
+req = urllib.request.Request(url, data)
+with urllib.request.urlopen(req) as response:
+   the_page = response.read()
