e6e2d7d90f97d77f9ed6b12d147cc596a7b57db2
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-03-02 17:24:12 +0200

    "hotfix"

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,12 +4,13 @@ import urllib.parse
 import urllib.request
 import sys
 
-email = {}
-url = sys.argv[1]
-email['email'] = sys.argv[2]
+if __name__ == '__main__':
+    email = {}
+    url = sys.argv[1]
+    email['email'] = sys.argv[2]
 
-data = urllib.parse.urlencode(email)
-data = data.encode('utf-8')
-req = urllib.request.Request(url, data)
-with urllib.request.urlopen(req) as response:
-   the_page = response.read()
+    data = urllib.parse.urlencode(email)
+    data = data.encode('utf-8')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        the_page = response.read()
