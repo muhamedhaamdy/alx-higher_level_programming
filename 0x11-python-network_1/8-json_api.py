30e4cceedbd2ebe41bfe6cfc4a7af04b3dc85739
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-03-03 04:24:47 +0200

    "hotfix"

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''sends a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter'''
+import requests
+import sys
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        q = ''
+    else:
+        q = sys.argv[1]
+    info = {'q': q}
+    r = requests.post('http://0.0.0.0:5000/search_user', data=info)
+    try:
+        res = r.json()
+        if not res:
+            print('No result')
+        else:
+            print('[{}] {}'.format(res['id'], res['name']))
+    except ValueError:
+        print('Not a valid JSON')
