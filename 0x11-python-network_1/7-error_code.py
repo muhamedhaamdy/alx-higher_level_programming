b233c2f82a8bd3057acfcf1bf86ea26db58e6779
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-03-03 03:52:45 +0200

    "hotfix"

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-"""script that takes in a URL, sends a request to the
-URL and displays the body of the response (decoded in utf-8). """
-import requests
+"""sends a request to the URL
+and displays the body of the response"""
 import sys
+import requests
 
-if __name__ == "__main__":
-    r = requests.get(sys.argv[1])
-    if r.status_code >= 400:
-        print("Error code: {}".format(r.status_code))
-    else:
-        print(r.text)
+
+r = requests.get(sys.argv[1])
+code = r.status_code
+if code >= 400:
+    print('Error code:', code)
+else:
+    print(r.text)
