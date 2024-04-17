5930716b6489f0669cab9da405cfb19da2313206
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-04-17 20:23:22 +0200

    "hotfix"

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,11 +1,38 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = process.argv.slice(2)[0];
 
-request(url, function (error, response, body) {
-  if (error) {
-    console.error(error);
+const url = process.argv[2];
+let count = 0;
+let completedRequests = 0;
+
+// Loop to make requests for films 0 to 6
+for (let i = 0; i < 7; i++) {
+  makeRequest(url + '/' + i);
+}
+
+function makeRequest (url) {
+  request(url, function (error, response, body) {
+    if (error) {
+      console.error(error);
+      checkCompletion();
+      return;
+    }
+
+    try {
+      const data = JSON.parse(body);
+      if (data.characters && Array.isArray(data.characters) && data.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+        count++;
+      }
+    } catch (parseError) {
+      console.error('Error parsing response body:', parseError);
+    }
+
+    checkCompletion();
+  });
+} function checkCompletion () {
+  completedRequests++;
+  if (completedRequests === 7) {
+    console.log(count);
   }
-});
-console.log(3);
+}
